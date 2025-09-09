742e22b01a19be0b9d9f0940eb74da7f6d95338e
Katherine <kaburbank@gmail.com>
2025-09-09 10:55:46 -0600

    Your commit message

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-//Write your code here
+//Declare object
 
+let attendee = {};
 
+// Add properties
+attendee["attendeeID"] = "T001"
+attendee["name"] = "Alice Smith"
+attendee["event"] = "JavaScript Conference"
+attendee["ticketType"] = "VIP"
+attendee.ticketPrice = 150.00
+
+console.log(attendee)
+
+// Creat a function to log attendee name
+function logAttendeeName(attendee) {
+  console.log(attendee["name"])
+}
+
+//Create a function to log Ticket Price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice)
+}
+
+//Create a function to update Ticket Type
+function updateTicketType(attendee, newTicketType) {
+attendee["ticketType"] = newTicketType
+}
+
+//Create a function to update Ticket Price
+function updateTicketPrice(attendee, newTicketPrice){
+attendee.ticketPrice = newTicketPrice
+}
+
+//Create a function to Remove the Event Property
+function removeEventProperty(attendee){
+delete attendee["event"]  
+}
+
+//Create a function to add a Checked-in Property
+function addCheckedInProperty(attendee){
+  attendee["Checked-in"] = true
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
